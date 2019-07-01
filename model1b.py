f7fe00f8692e3450e442083416ed01535cb7370f
klebster2 <42589399+klebster2@users.noreply.github.com>
2019-07-01 09:24:57 +0100

    Add files via upload

--- model1b.py
+++ model1b.py
@@ -13,6 +13,7 @@ import os, pdb, argparse, re, random
 from tqdm import tqdm
 from collections import defaultdict, OrderedDict
 
+from time import time
 import torch, torch.nn as nn, torch.optim as optim
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
 from torch.autograd import Variable
@@ -82,13 +83,13 @@ class BuildModel(nn.ModuleList):
                 if re.match('ema_.*.txt',d2): 
                     with open(os.path.join(directory, d1, d2), 'r') as f:
                         self.norm = re.findall(ema_norm_str,f.readlines()[0])
-                    if 'means' in d2: data_dict.update({key:self.load_means()})
-                    elif 'stds' in d2:data_dict.update({key:self.load_4xSTDs()})
+                    if 'means' in d2: data_dict.update({key:torch.FloatTensor(self.load_means())})
+                    elif 'stds' in d2:data_dict.update({key:torch.FloatTensor(self.load_4xSTDs())})
                     continue
                 np_array = np.load(os.path.join(directory, d1, d2), 
                                    allow_pickle = True)
                 if d1=='channel_name': data_dict.update({key:np_array})
-                else:  data_dict[d1].update({key:np_array})
+                else:  data_dict[d1].update({key:torch.FloatTensor(np_array)})
         return data_dict
 
     def forward(self, x, hc):
@@ -98,15 +99,22 @@ class BuildModel(nn.ModuleList):
             tuple of hidden and cell state
         """
         # empty tensor for the output of the lstm
+        t7=time()
         output_seq = torch.empty((self.sequence_len,
                                   self.batch_size,
                                   self.output_dim))
-        packed_outputs, (h, c) = self.bidirlstm(x.float())
+        t8=time()
+        packed_outputs, (h, c) = self.bidirlstm(x)
         # repad (unpack) padded outputs
+        t9=time()
         lstm_output, _ = pad_packed_sequence(packed_outputs, 
                                              batch_first=True, 
                                              total_length=self.sequence_len)
+        t10=time()
         output_seq = self.fc(lstm_output)
+        t11=time()
+        del lstm_output, packed_outputs, h, c
+        #print('{:.4}\t{:.4}\t{:.4}\t{:.4}'.format(t8-t7, t9-t8, t10-t9, t11-t10))
         return output_seq
     
     def init_hidden(self): # initialize hidden state and cell state to zeros
@@ -140,25 +148,31 @@ def get_batches(in_dict, batch_size=None, shuffle=False, net_in=False):
             batches.append((padded_seq_batch, batch))
     return batches
 
-def unnormalize_output(batches, norm_data):
-    pdb.set_trace()
+def unnormalize_output(batches, norm_data, batches_set=True):
     unnormalized_data = []
-    for b in batches:
+    if batches_set==True:
+        for b in batches:
+            norm = torch.FloatTensor(norm_data).repeat([*b.shape][0],[*b.shape][1],1)
+            assert norm.shape == b.shape
+            unnormalized_data.append(torch.mul(norm, b))
+        return unnormalized_data
+    else:
+        b = batches
         norm = torch.FloatTensor(norm_data).repeat([*b.shape][0],[*b.shape][1],1)
-        assert norm.shape == b.shape
-        unnormalized_data.append(torch.mul(norm, b))
-    
-    return unnormalized_data
+        return torch.mul(norm, b)
+
+
 
 def run():
     """
     
     """
+    torch.backends.cudnn.benchmark=True
     net = BuildModel(args)
     # define the loss and the optimizer
-    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    optimizer = optim.Adam(net.parameters(), lr=0.01)
     criterion, criterion_mm = nn.MSELoss(), nn.MSELoss()
-    
+
     # get batches for train/ validation sets
     sample_number = len(net.lsf_dict['channel_lsfd'].values())
     val_idx = int(sample_number * (1 - 0.1)) # validation 10%, training 90%
@@ -169,58 +183,57 @@ def run():
     tt = {k:net.ema_dict['channel_ema'][k]  for k in samples[:val_idx]}
     vt = {k:net.ema_dict['channel_ema'][k]  for k in samples[val_idx:]}
     print('getting batches...')
+    
     # define batch training (t) and validation (v) sets for source (s) and target (t)
     batch_ts  = get_batches(ts, batch_size=6, shuffle=False, net_in=True)
     batch_vs  = get_batches(vs, batch_size=6, shuffle=False, net_in=True)
     
     batch_tt  = get_batches(tt, batch_size=6, shuffle=False, net_in=False)
     batch_vt  = get_batches(vt, batch_size=6, shuffle=False, net_in=False)
-    batch_ttm = unnormalize_output(return_first(batch_tt), net.ema_dict['ema_stds'])
-    batch_vtm = unnormalize_output(return_first(batch_vt), net.ema_dict['ema_stds'])
+    batch_ttm = unnormalize_output(return_first(batch_tt), net.ema_dict['ema_stds'], True)
+    batch_vtm = unnormalize_output(return_first(batch_vt), net.ema_dict['ema_stds'], True)
     #[FloatTensor(i) for k,i in net.lsf_dict['channel_lsfd'].items()]
     count=int(0)
-    for epoch in range(10):
+    t0=time()
+    for epoch in range(100):
         # reinit the hidden and cell states
-        hc = net.init_hidden()
         # Get batches: 1) lsf ready for input & 2a) ema norm & 2b) ema mm for target output
         for s, t, tm in tqdm(zip(batch_ts, batch_tt, batch_ttm)):
+            hc = net.init_hidden()
+            t1=time()
             src_pkd_seq, batch = s
             tgt_seq, _ = t
-            pdb.set_trace()
+            #pdb.set_trace()
             count += 1
             net.get_len(max(get_seq_lens(batch)))
-            # get the torch tensors training data
-            #batch_targets = pad_sequence(tensortargets, batch_first=True)
             # zero out the gradients
             optimizer.zero_grad()
-            # get the output sequence from the input and the initial hidden and cell states
-            output = net(src_pkd_seq.float(), hc)
-
-            #normalized_stds = torch.stack([torch.FloatTensor(net.ema_dict['ema_stds']) for i in range(output.size(0))])
-            
-#            for o, n, index in zip(output, normalized_stds, range(output.shape[0])):
-#                print(o.shape, n.shape)
-#                output_mm.append(torch.FloatTensor([i*j for i,j in zip(o.tolist(),n.tolist())]))
-#                target_mm.append(torch.FloatTensor([i*j for i,j in zip(targets[index].tolist(),n.tolist())]))
-#            
-#            output_mm=torch.stack(output_mm)
-#            target_mm=torch.stack(target_mm)
-            # calculate the loss
+            t2=time()
+            output = net(src_pkd_seq, hc)
+            t3=time()
             # we need to calculate the loss across all batches, so we have to flat the targets tensor
-            output_mm = unnormalize_output(output, net.ema_dict['ema_stds'])
-            
+            #print(type(tgt_seq), type(output), type(src_pkd_seq))
+            output_mm = unnormalize_output(output, net.ema_dict['ema_stds'], False)
+            t4=time()
             loss       =    criterion(output, tgt_seq)
+            t5=time()
             loss_mm    =    criterion_mm(output_mm ,tgt_seq)
-            print('count: {}, loss:    {}, '.format(count, loss.item(tgt_seq)))
-            print('count: {}, loss_mm: {}, '.format(count, loss_mm.item()*1000))
-            # calculate the gradients
             
+            print('count: {},\tloss:\t{:.5},\tloss_mm: {:.7}'.format(count, loss.item(), loss_mm.item()*1000))
+            # calculate the gradients
             # update the parameters of the model
+            loss.backward()
             optimizer.step()
-        
+            t6=time()
+            #print('{:.4}\t{:.4}\t{:.4}\t{:.4}\t{:.4}'.format(t2-t1, t3-t2, t4-t3, t5-t4, t6-t5))
+            del loss, loss_mm, output, output_mm
             # feedback every 10 batches:     val_data_batches   = get_batches(val_data, 6, False)
-            #if count % 10 == 0: print('count: {}, loss: {}, '.format(count, loss.item()))
-
+#            if count % 10 == 0:             
+#                print('count: {}, loss:    {:.5}, '.format(count, loss.item()))
+#                print('count: {}, loss_mm: {:.7}, '.format(count, loss_mm.item()*1000))
+    tend=time()
+    print(tend-t0)
+    
 def main(args):
     run()
 
